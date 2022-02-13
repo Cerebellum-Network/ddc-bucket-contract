693bb0c3e1fc08ef2bbceb15a2b9f4f011b71531
Aurélien Nicolas <info@nau.re>
2022-02-13 19:40:22 +0100

    Move bucket contract in a directory

--- build-all.sh
+++ build-all.sh
@@ -4,6 +4,7 @@ set -eu
 
 cargo +nightly test --workspace
 
+cargo +nightly contract build --manifest-path bucket/Cargo.toml
 cargo +nightly contract build --manifest-path cluster/Cargo.toml
 cargo +nightly contract build --manifest-path payments/Cargo.toml
 cargo +nightly contract build
