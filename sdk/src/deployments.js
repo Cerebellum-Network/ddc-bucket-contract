4f3988d858bcc2b78d435628f1a515cc81127c87
Aurélien Nicolas <info@nau.re>
2022-04-05 16:21:39 +0200

    deployment

--- sdk/src/deployments.js
+++ sdk/src/deployments.js
@@ -5,9 +5,9 @@ const {registerContract} = require("./contractRegistry.js");
 const ddc_bucket_abi = require("./abi/ddc_bucket.json");
 
 function init() {
-    registerABI("ddc_bucket", ddc_bucket_abi, "0x88c54aa08d5e07ea623486675b4bd471c6d0f34d481efecd2089a7ff1aba6f6a");
+    registerABI("ddc_bucket", ddc_bucket_abi, "0xdf34b3cb3607cadac49e363a787dfb1e74b47b71af0e375a4eb89f33c1168edf");
 
-    registerContract("ddc_bucket", "Cere Testnet", "5GX6o5uijAYy1h5NSMfpYFeoEUb2iAnepgqgmjyqFsURoxNE");
+    registerContract("ddc_bucket", "Cere Testnet", "5DHey2c1XfgCjiVLddwG46mhCUMSw7iSL2a8TxRZR1SjezPX");
 }
 
 
