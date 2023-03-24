302611055e8b4fecd51e97c4df7adf75632c26dd
Vladimir Patsenko <v@patsenko.ru>
2023-03-24 16:02:02 +0400

    test removed

--- bucket/ddc_bucket/tests/mod.rs
+++ bucket/ddc_bucket/tests/mod.rs
@@ -4,11 +4,3 @@ mod test_admin;
 mod test_contract;
 mod test_currency;
 // mod test_committer;
-
-mod as_cluster_manager;
-mod as_gateway;
-mod as_storage;
-mod as_user;
-mod node;
-mod test_network;
-mod topology;
